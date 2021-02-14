23d0bfc74b75623e5b33db350346a0812314bc2e
mlee2328 <79023012+mlee2328@users.noreply.github.com>
2021-02-13 16:38:48 -0800

    Add files via upload

--- Player Class.java
+++ Player Class.java
@@ -3,23 +3,26 @@ class Player{
     private double money;
     private int age;
     private int hand21; 
+    private String status;
 
     public Player(){
         name = "";
         money = 0;
         age = 0;
         hand21 = 0;
+        status = "alive";
     }
     public Player(String n){
         name = n;
         money = 0;
         age = 0;
         hand21 = 0;
+        status = "alive";
     }
     public void setName(String n){name = n;}
     public void setMoney(double m){money = m;}
     public void addMoney(double m){money+=m;}
-    public void subtractMoney(double m){
+    public void subtractMoney(double m){//debt?
         if (m>money){money=0;}
         else{money-=m;}
     }
@@ -35,8 +38,10 @@ class Player{
     }
     public void setHand21(int num){hand21=num;}
     public void addHand21(int num){hand21+=num;}
+    public void setStatus(String s){status=s;}
     public String getName(){return name;}
     public double getMoney(){return money;}
     public int getAge(){return age;}
     public int getHand21(){return hand21;}
+    public String getStatus(){return status;}
 }
